d941bf386cd5bc41f72eae97b7fbb86401024819
DarthPellegrini <pellegrinidev@outlook.com>
2020-11-17 10:39:34 -0300

    hotfix 3 return of the king

--- src/main/java/com/unisc/moedas/FXMLController.java
+++ src/main/java/com/unisc/moedas/FXMLController.java
@@ -29,81 +29,114 @@ public class FXMLController implements Initializable {
 																   "Superior esquerdo","Superior central","Superior direito",
 																   "Inferior esquerdo","Inferior central","Inferior direito",
 															   	   "Não está na borda, mas dentro da moeda");
+	private static List<String> modalidadeData =  Arrays.asList("Olímpica","Paraolímpica");
 	
 	private static double[][][] moedasValues = {
 			//moedas de 2014 (1 ambígua)
 			{{0.0,0.0,1.0,0.0,0.0,
 			  0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,
-			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,1.0,0.0,0.0,
 			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,
-			  0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0},{1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0},{1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,1.0,0.0,0.0,
 			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,
-			  0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0},{1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,1.0,0.0,0.0,
 			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
 			//moedas de 2015-1
 			{{0.0,0.0,0.0,1.0,0.0,
 			  0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,0.0,1.0,0.0,
 			  0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,0.0,1.0,0.0,
 			  0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,0.0,1.0,0.0,
 			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
 			//moedas de 2015-2 (2\/ ambígua (L e B) 4\/ ambígua)
 			{{0.0,0.0,0.0,1.0,0.0,
 			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,0.0,1.0,0.0,
 			  0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,
-			  0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,0.0,1.0,0.0,
 			  0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,
-			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0}},	
+			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,0.0,1.0,0.0,
 			  0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0}},
+			  0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0}},
 			//moedas de 2016 (4 ambígua)
 			{{0.0,0.0,0.0,0.0,1.0,
 			  0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0}},	
+			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0}},	
 			{{0.0,0.0,0.0,0.0,1.0,
 			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0}},	
+			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0}},	
 			{{0.0,0.0,0.0,0.0,1.0,
 			  0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0}},	
+			  0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0}},	
 			{{0.0,0.0,0.0,0.0,1.0,
 			  0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0}},
+			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0}},
 			//moeda da bandeira especial
 			{{0.0,1.0,0.0,0.0,0.0,
 			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,
-			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0},{1.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
-			//moeda que não é
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0},{1.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			//moeda que não são comemorativas
 			{{1.0,0.0,0.0,0.0,0.0,
 			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
 			{{0.0,0.0,0.0,0.0,0.0,
 			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
 			{{0.0,0.0,0.0,0.0,0.0,
 			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
 			{{1.0,0.0,0.0,0.0,0.0,
 			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
-			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			{{1.0,0.0,0.0,0.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			{{0.0,0.0,0.0,0.0,0.0,
+			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			{{0.0,0.0,0.0,0.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			{{1.0,0.0,0.0,0.0,0.0,
+			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+			  1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0},{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			//moedas ambíguas
+			{{0.0,0.0,1.0,0.0,0.0,
+			  0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,
+			  0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},{1.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+				
+			{{0.0,0.0,0.0,1.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,
+			  0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			{{0.0,0.0,0.0,1.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			{{0.0,0.0,0.0,1.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0}},
+			
+			{{0.0,0.0,0.0,1.0,0.0,
+			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+			  0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0}},
+			
+			{{0.0,0.0,0.0,0.0,1.0,
+			  0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,
+			  0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0},{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0}},
 			
 	};
-	
-	private double[] resultado;
 	
 	@FXML
 	private Label titulo;
@@ -117,11 +150,14 @@ public class FXMLController implements Initializable {
 	@FXML
 	private ChoiceBox<String> posicaoBrasil;
 	
+	@FXML
+	private ChoiceBox<String> modalidade;
+	
     @FXML
     private void btnOnClick(ActionEvent event) {
     	if (validaRespostas())
 	    	try {
-				resultado = neuralNetwork.Recognize(getRespostas());
+				double[] resultado = neuralNetwork.Recognize(getRespostas());
 	    		showResultScreen(classificaMoedaCompleto(resultado));
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -144,35 +180,14 @@ public class FXMLController implements Initializable {
     	alert.setTitle("Resultados");
     	alert.setHeaderText("Observa-se a seguir as saídas da rede neural:");
     	alert.setContentText(resultado);
+    	alert.setResizable(true);
+    	alert.setWidth(450);
     	alert.showAndWait();
-    }
-    
-    private String classificaMoeda(double[] resultado) {
-    	return ("É uma moeda comemorativa? " + formataResultado(resultado[0]) + 
-    			((resultado[0]*100 >= 90 ) ?
-    			(((resultado[0]*100.0 >= 90) ? ("\nRaridade: " + (resultado[1]*100.0 >= 90 ? "É muito rara " : "Tem uma certa raridade ") + String.format("%1.4f%%", (resultado[1]*100.0)) ) : "") + 
-				((resultado[2]*100.0 >= 90) ? "\nMoeda especial (Bandeira): " + formataResultado(resultado[2]) : "") +
-				((resultado[3]*100.0 >= 90) ? "\nModalidade (Atletismo): " + formataResultado(resultado[3]) : "") +
-				((resultado[4]*100.0 >= 90) ? "\nModalidade (Natação): " + formataResultado(resultado[4]) : "") +
-				((resultado[5]*100.0 >= 90) ? "\nModalidade (Paratriatlo): " + formataResultado(resultado[5]) : "") +
-				((resultado[6]*100.0 >= 90) ? "\nModalidade (Golfe): " + formataResultado(resultado[6]) : "") +
-				((resultado[7]*100.0 >= 90) ? "\nModalidade (Basquetebol): " + formataResultado(resultado[7]) : "") +
-				((resultado[8]*100.0 >= 90) ? "\nModalidade (Vela): " + formataResultado(resultado[8]) : "") +
-				((resultado[9]*100.0 >= 90) ? "\nModalidade (Paracanoagem): " + formataResultado(resultado[9]) : "") +
-				((resultado[10]*100.0 >= 90) ? "\nModalidade (Rugby): " + formataResultado(resultado[10]) : "") +
-				((resultado[11]*100.0 >= 90) ? "\nModalidade (Futebol): " + formataResultado(resultado[11]) : "") +
-				((resultado[12]*100.0 >= 90) ? "\nModalidade (Voleibol): " + formataResultado(resultado[12]) : "") +
-				((resultado[13]*100.0 >= 90) ? "\nModalidade (Atletismo Paralímpico): " + formataResultado(resultado[13]) : "") +
-				((resultado[14]*100.0 >= 90) ? "\nModalidade (Judô): " + formataResultado(resultado[14]) : "") +
-				((resultado[15]*100.0 >= 90) ? "\nModalidade (Boxe): " + formataResultado(resultado[15]) : "") + 
-				((resultado[16]*100.0 >= 90) ? "\nModalidade (Natação Paralímpica): " + formataResultado(resultado[16]) : "") +
-				((resultado[17]*100.0 >= 90) ? "\nMascote (Tom): " + formataResultado(resultado[17]) : "") +
-				((resultado[18]*100.0 >= 90) ? "\nMascote (Vinícius): " + formataResultado(resultado[18]) : "")) : ""));
     }
     
     private String classificaMoedaCompleto(double[] resultado) {
     	return ("É uma moeda comemorativa?           " + formataResultado(resultado[0]) + 
-    			"\nRaridade:                                            " + (resultado[1]*100.0 >= 90 ? "É muito rara" : "Tem uma certa raridade") + String.format(" (%1.4f%%)", (resultado[1]*100.0)) + 
+    			"\nRaridade:                                            " + (resultado[1]*100.0 >= 90 ? "É muito rara" : "É pouco rara") + String.format(" (%1.4f%%)", (resultado[1]*100.0)) + 
 				"\nMoeda especial (Bandeira):                " + formataResultado(resultado[2]) +
 				"\nModalidade (Atletismo):                     " + formataResultado(resultado[3]) +
 				"\nModalidade (Natação):                       " + formataResultado(resultado[4]) +
@@ -200,14 +215,19 @@ public class FXMLController implements Initializable {
     private boolean validaRespostas() {
     	return (anoGravadoGetResposta() >= 0.0 &&
     			posicaoLogotipoGetResposta() >= 0.0 &&
-    			posicaoBrasilGetResposta() >= 0.0);
+    			posicaoBrasilGetResposta() >= 0.0 && 
+    			modalidadeGetResposta() >= 0.0);
     }
     
     private double[] getRespostas() {
-    	double values[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    	double values[] = {0.0,0.0,0.0,0.0,0.0, 
+    					   0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0, 
+    					   0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0, 
+    					   0.0};
     	values[anoGravadoGetResposta()] = 1.0;
     	values[posicaoLogotipoGetResposta()+5] = 1.0;
     	values[posicaoBrasilGetResposta()+15] = 1.0;
+    	values[23] = modalidadeGetResposta()*1.0;
     	return values;
     }
     
@@ -222,6 +242,10 @@ public class FXMLController implements Initializable {
     private int posicaoBrasilGetResposta() {
     	return posicaoBrasilData.indexOf(posicaoBrasil.getValue());
     }
+    
+    private int modalidadeGetResposta() {
+    	return modalidadeData.indexOf(modalidade.getValue());
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -229,42 +253,19 @@ public class FXMLController implements Initializable {
         anoGravado.setItems((ObservableList<String>) FXCollections.observableArrayList(anoGravadoData));
         posicaoLogotipo.setItems((ObservableList<String>) FXCollections.observableArrayList(posicaoLogotipoData));
         posicaoBrasil.setItems((ObservableList<String>) FXCollections.observableArrayList(posicaoBrasilData));
+        modalidade.setItems((ObservableList<String>) FXCollections.observableArrayList(modalidadeData));
         
         try {
-        	neuralNetwork = new Backpropagation(23, 19);
+        	neuralNetwork = new Backpropagation(24, 19);
             
-            DataSet trainingSet = new DataSet(23, 19);
+            DataSet trainingSet = new DataSet(24, 19);
         	
-			trainingSet.Add(new DataSetObject(moedasValues[0][0], moedasValues[0][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[1][0], moedasValues[1][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[2][0], moedasValues[2][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[3][0], moedasValues[3][1]));
-			
-			trainingSet.Add(new DataSetObject(moedasValues[4][0], moedasValues[4][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[5][0], moedasValues[5][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[6][0], moedasValues[6][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[7][0], moedasValues[7][1]));
-			
-			trainingSet.Add(new DataSetObject(moedasValues[8][0], moedasValues[8][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[9][0], moedasValues[9][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[10][0], moedasValues[10][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[11][0], moedasValues[11][1]));
-			
-			trainingSet.Add(new DataSetObject(moedasValues[12][0], moedasValues[12][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[13][0], moedasValues[13][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[14][0], moedasValues[14][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[15][0], moedasValues[15][1]));
-			
-			trainingSet.Add(new DataSetObject(moedasValues[16][0], moedasValues[16][1]));
-			
-			trainingSet.Add(new DataSetObject(moedasValues[17][0], moedasValues[17][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[18][0], moedasValues[18][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[19][0], moedasValues[19][1]));
-			trainingSet.Add(new DataSetObject(moedasValues[20][0], moedasValues[20][1]));
-			
-			neuralNetwork.SetMaxIterationNumber(50000);
-			neuralNetwork.SetLearningRate(0.0001);
-			neuralNetwork.SetErrorRate(0.0005);
+            for (int i = 0; i < moedasValues.length; i++) 
+            	trainingSet.Add(new DataSetObject(moedasValues[i][0], moedasValues[i][1]));
+            
+			neuralNetwork.SetMaxIterationNumber(5000);
+			neuralNetwork.SetLearningRate(0.01);
+			neuralNetwork.SetErrorRate(0.005);
 			neuralNetwork.Learn(trainingSet);
 		} catch (Exception e) {
 			e.printStackTrace();
